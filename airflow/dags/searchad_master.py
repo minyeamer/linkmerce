03fb7acbaf0cfa43bded1d716d306c6e1ba63359
minyeamer <minyeamer@gmail.com>
2025-09-16 01:14:14 +0900

    airflow: refactor searchad DAGs with task groups - group existing searchad tasks into a Task Group. - add a new Task Group for GFA tasks.

--- airflow/dags/searchad_master.py
+++ airflow/dags/searchad_master.py
@@ -1,83 +1,164 @@
-from airflow.sdk import DAG, task
+from airflow.sdk import DAG, TaskGroup, task
+from airflow.utils.trigger_rule import TriggerRule
 from datetime import timedelta
 import pendulum
 
 
 with DAG(
     dag_id = "searchad_master",
-    schedule = "40 5 * * *",
+    schedule = "20 5 * * *",
     start_date = pendulum.datetime(2025, 8, 30, tz="Asia/Seoul"),
-    dagrun_timeout = timedelta(minutes=10),
+    dagrun_timeout = timedelta(minutes=20),
     catchup = False,
-    tags = ["priority:medium", "searchad:report", "api:searchad", "schedule:daily", "time:morning"],
+    tags = ["priority:medium", "searchad:report", "api:searchad", "login:gfa", "schedule:daily", "time:morning"],
 ) as dag:
 
-    PATH = ["searchad", "api", "searchad_master"]
+    with TaskGroup(group_id="api_group") as api_group:
 
-    @task(task_id="read_variables", retries=3, retry_delay=timedelta(minutes=1))
-    def read_variables() -> dict:
-        from variables import read
-        return read(PATH, tables=True, service_account=True)
+        API_PATH = ["searchad", "api", "searchad_master"]
 
-    @task(task_id="read_credentials", retries=3, retry_delay=timedelta(minutes=1))
-    def read_credentials() -> list:
-        from variables import read
-        return read(PATH, credentials=True)["credentials"]
+        @task(task_id="read_api_variables", retries=3, retry_delay=timedelta(minutes=1))
+        def read_api_variables() -> dict:
+            from variables import read
+            return read(API_PATH, tables=True, service_account=True)
+
+        @task(task_id="read_api_credentials", retries=3, retry_delay=timedelta(minutes=1))
+        def read_api_credentials() -> list:
+            from variables import read
+            return read(API_PATH, credentials=True)["credentials"]
 
 
-    @task(task_id="etl_searchad_master", map_index_template="{{ credentials['customer_id'] }}")
-    def etl_searchad_master(credentials: dict, variables: dict, data_interval_end: pendulum.DateTime = None, **kwargs) -> dict:
-        from_date = str(data_interval_end.in_timezone("Asia/Seoul").subtract(days=365*2).date())
-        return {api_type: main(api_type, **credentials, from_date=from_date, **variables) for api_type in ["campaign", "adgroup", "ad"]}
+        @task(task_id="etl_searchad_master", map_index_template="{{ credentials['customer_id'] }}")
+        def etl_searchad_master(credentials: dict, variables: dict, data_interval_end: pendulum.DateTime = None, **kwargs) -> dict:
+            from_date = str(data_interval_end.in_timezone("Asia/Seoul").subtract(days=365*2).date())
+            return {api_type: main_api(api_type, **credentials, from_date=from_date, **variables) for api_type in ["campaign", "adgroup", "ad"]}
 
-    def main(
-            api_type: str,
-            api_key: str,
-            secret_key: str,
-            customer_id: int | str,
-            from_date: str,
-            service_account: dict,
-            tables: dict[str,str],
-            merge: dict[str,dict],
-            **kwargs
-        ) -> dict:
-        from linkmerce.common.load import DuckDBConnection
-        from linkmerce.extensions.bigquery import BigQueryClient
-        from importlib import import_module
-        import logging
-        extract = getattr(import_module("linkmerce.api.searchad.api"), api_type)
+        def main_api(
+                api_type: str,
+                api_key: str,
+                secret_key: str,
+                customer_id: int | str,
+                from_date: str,
+                service_account: dict,
+                tables: dict[str,str],
+                merge: dict[str,dict],
+                **kwargs
+            ) -> dict:
+            from linkmerce.common.load import DuckDBConnection
+            from linkmerce.extensions.bigquery import BigQueryClient
+            from importlib import import_module
+            import logging
+            extract = getattr(import_module("linkmerce.api.searchad.api"), api_type)
 
-        with DuckDBConnection(tzinfo="Asia/Seoul") as conn:
-            extract(
-                api_key = api_key,
-                secret_key = secret_key,
-                customer_id = customer_id,
-                from_date = from_date,
-                connection = conn,
-                return_type = "none",
-            )
-            logging.info(f"[{customer_id}] API request completed for downloading the {api_type} master report")
-
-            with BigQueryClient(service_account) as client:
-                return dict(
-                    params = dict(
-                        customer_id = customer_id,
-                        from_date = from_date,
-                    ),
-                    count = dict(
-                        data = conn.count_table("data"),
-                    ),
-                    status = dict(
-                        data = client.merge_into_table_from_duckdb(
-                            connection = conn,
-                            source_table = "data",
-                            staging_table = f'{tables[f"temp_{api_type}"]}_{customer_id}',
-                            target_table = tables[api_type],
-                            **merge[api_type],
-                            progress = False,
-                        ),
-                    ),
+            with DuckDBConnection(tzinfo="Asia/Seoul") as conn:
+                extract(
+                    api_key = api_key,
+                    secret_key = secret_key,
+                    customer_id = customer_id,
+                    from_date = from_date,
+                    connection = conn,
+                    return_type = "none",
                 )
+                logging.info(f"[{customer_id}] API request completed for downloading the {api_type} master report")
+
+                with BigQueryClient(service_account) as client:
+                    return dict(
+                        params = dict(
+                            customer_id = customer_id,
+                            from_date = from_date,
+                        ),
+                        count = dict(
+                            data = conn.count_table("data"),
+                        ),
+                        status = dict(
+                            data = client.merge_into_table_from_duckdb(
+                                connection = conn,
+                                source_table = "data",
+                                staging_table = f'{tables[f"temp_{api_type}"]}_{customer_id}',
+                                target_table = tables[api_type],
+                                **merge[api_type],
+                                progress = False,
+                            ),
+                        ),
+                    )
 
 
-    etl_searchad_master.partial(variables=read_variables()).expand(credentials=read_credentials())
+        (etl_searchad_master
+            .partial(variables=read_api_variables())
+            .expand(credentials=read_api_credentials()))
+
+
+    with TaskGroup(group_id="gfa_group") as gfa_group:
+
+        GFA_PATH = ["searchad", "gfa", "searchad_master"]
+
+        @task(task_id="read_gfa_variables", retries=3, retry_delay=timedelta(minutes=1), trigger_rule=TriggerRule.ALWAYS)
+        def read_gfa_variables() -> dict:
+            from variables import read
+            return read(GFA_PATH, tables=True, service_account=True)
+
+        @task(task_id="read_gfa_credentials", retries=3, retry_delay=timedelta(minutes=1), trigger_rule=TriggerRule.ALWAYS)
+        def read_gfa_credentials() -> list:
+            from variables import read
+            return read(GFA_PATH, credentials=True)["credentials"]
+
+
+        @task(task_id="etl_gfa_master", map_index_template="{{ credentials['account_no'] }}")
+        def etl_gfa_master(credentials: dict, variables: dict, **kwargs) -> dict:
+            return {api_type: main_gfa(api_type, **credentials, **variables) for api_type in ["campaign", "adset", "creative"]}
+
+        def main_gfa(
+                api_type: str,
+                account_no: int | str,
+                cookies: str,
+                service_account: dict,
+                tables: dict[str,str],
+                merge: dict[str,dict],
+                **kwargs
+            ) -> dict:
+            from linkmerce.common.load import DuckDBConnection
+            from linkmerce.extensions.bigquery import BigQueryClient
+            from importlib import import_module
+            import logging
+            extract = getattr(import_module("linkmerce.api.searchad.gfa"), api_type)
+            status = [("RUNNABLE" if api_type == "campaign" else "ALL"), "DELETED"]
+
+            with DuckDBConnection(tzinfo="Asia/Seoul") as conn:
+                extract(
+                    account_no = account_no,
+                    cookies = cookies,
+                    status = status,
+                    connection = conn,
+                    progress = False,
+                    return_type = "none",
+                )
+                logging.info(f"[{account_no}] request completed for downloading the {api_type} list")
+
+                with BigQueryClient(service_account) as client:
+                    return dict(
+                        params = dict(
+                            account_no = account_no,
+                            status = status,
+                        ),
+                        count = dict(
+                            data = conn.count_table("data"),
+                        ),
+                        status = dict(
+                            data = client.merge_into_table_from_duckdb(
+                                connection = conn,
+                                source_table = "data",
+                                staging_table = f'{tables[f"temp_{api_type}"]}_{account_no}',
+                                target_table = tables[api_type],
+                                **merge[api_type],
+                                progress = False,
+                            ),
+                        ),
+                    )
+
+
+        (etl_gfa_master
+            .partial(variables=read_gfa_variables())
+            .expand(credentials=read_gfa_credentials()))
+
+
+    api_group >> gfa_group
--- airflow/dags/searchad_report.py
+++ airflow/dags/searchad_report.py
@@ -1,93 +1,159 @@
-from airflow.sdk import DAG, task
+from airflow.sdk import DAG, TaskGroup, task
+from airflow.utils.trigger_rule import TriggerRule
 from datetime import timedelta
 import pendulum
 
 
 with DAG(
     dag_id = "searchad_report",
-    schedule = "50 5 * * *",
+    schedule = "40 5 * * *",
     start_date = pendulum.datetime(2025, 8, 24, tz="Asia/Seoul"),
-    dagrun_timeout = timedelta(minutes=10),
+    dagrun_timeout = timedelta(minutes=20),
     catchup = False,
-    tags = ["priority:medium", "searchad:report", "login:searchad", "schedule:daily", "time:morning"],
+    tags = ["priority:medium", "searchad:report", "login:searchad", "login:gfa", "schedule:daily", "time:morning"],
 ) as dag:
 
-    PATH = ["searchad", "manage", "searchad_report"]
+    with TaskGroup(group_id="searchad_group") as searchad_group:
 
-    @task(task_id="read_variables", retries=3, retry_delay=timedelta(minutes=1))
-    def read_variables() -> dict:
-        from variables import read
-        return read(PATH, tables=True, service_account=True)
+        SEARCHAD_PATH = ["searchad", "manage", "searchad_report"]
 
-    @task(task_id="read_queries", retries=3, retry_delay=timedelta(minutes=1))
-    def read_queries() -> list:
-        from variables import read
-        variables = read(PATH, credentials=True)
-        params = variables["params"]
-        credentials = filter_valid_accounts(variables["credentials"])
-        return [dict(credential, **params[credential["customer_id"]]) for credential in credentials]
+        @task(task_id="read_searchad_variables", retries=3, retry_delay=timedelta(minutes=1))
+        def read_searchad_variables() -> dict:
+            from variables import read
+            return read(SEARCHAD_PATH, tables=True, service_account=True)
 
-    def filter_valid_accounts(credentials: list[dict]) -> list[dict]:
-        from linkmerce.api.searchad.manage import has_permission
-        accounts = [credential for credential in credentials if has_permission(**credential)]
-        if credentials and (not accounts):
-            from airflow.exceptions import AirflowFailException
-            raise AirflowFailException("At least one account does not have permissions")
-        return accounts
+        @task(task_id="read_searchad_queries", retries=3, retry_delay=timedelta(minutes=1))
+        def read_searchad_queries() -> list:
+            from variables import read
+            variables = read(SEARCHAD_PATH, credentials=True)
+            params = variables["params"]
+            return [dict(credential, **params[credential["customer_id"]]) for credential in variables["credentials"]]
 
 
-    @task(task_id="etl_searchad_report", map_index_template="{{ queries['customer_id'] }}")
-    def etl_searchad_report(queries: dict, variables: dict, data_interval_end: pendulum.DateTime = None, **kwargs) -> dict:
-        date = str(data_interval_end.in_timezone("Asia/Seoul").subtract(days=1).date())
-        return main(**queries, date=date, **variables)
+        @task(task_id="etl_searchad_report", map_index_template="{{ queries['customer_id'] }}")
+        def etl_searchad_report(queries: dict, variables: dict, data_interval_end: pendulum.DateTime = None, **kwargs) -> dict:
+            date = str(data_interval_end.in_timezone("Asia/Seoul").subtract(days=1).date())
+            return main(**queries, date=date, **variables)
 
-    def main(
-            customer_id: int | str,
-            cookies: str,
-            report_id: str,
-            report_name: str,
-            date: str,
-            userid: str,
-            service_account: dict,
-            tables: dict[str,str],
-            **kwargs
-        ) -> dict:
-        from linkmerce.common.load import DuckDBConnection
-        from linkmerce.api.searchad.manage import daily_report
-        from linkmerce.extensions.bigquery import BigQueryClient
+        def main(
+                customer_id: int | str,
+                cookies: str,
+                report_id: str,
+                report_name: str,
+                date: str,
+                userid: str,
+                service_account: dict,
+                tables: dict[str,str],
+                **kwargs
+            ) -> dict:
+            from linkmerce.common.load import DuckDBConnection
+            from linkmerce.api.searchad.manage import daily_report
+            from linkmerce.extensions.bigquery import BigQueryClient
 
-        with DuckDBConnection(tzinfo="Asia/Seoul") as conn:
-            daily_report(
-                customer_id = customer_id,
-                cookies = cookies,
-                report_id = report_id,
-                report_name = report_name,
-                userid = userid,
-                start_date = date,
-                connection = conn,
-                return_type = "none",
-            )
-
-            with BigQueryClient(service_account) as client:
-                return dict(
-                    params = dict(
-                        customer_id = customer_id,
-                        report_id = report_id,
-                        report_name = report_name,
-                        date = date,
-                    ),
-                    count = dict(
-                        data = conn.count_table("data"),
-                    ),
-                    status = dict(
-                        data = client.load_table_from_duckdb(
-                            connection = conn,
-                            source_table = "data",
-                            target_table = tables["data"],
-                            progress = False,
-                        ),
-                    ),
+            with DuckDBConnection(tzinfo="Asia/Seoul") as conn:
+                daily_report(
+                    customer_id = customer_id,
+                    cookies = cookies,
+                    report_id = report_id,
+                    report_name = report_name,
+                    userid = userid,
+                    start_date = date,
+                    connection = conn,
+                    return_type = "none",
                 )
 
+                with BigQueryClient(service_account) as client:
+                    return dict(
+                        params = dict(
+                            customer_id = customer_id,
+                            report_id = report_id,
+                            report_name = report_name,
+                            date = date,
+                        ),
+                        count = dict(
+                            data = conn.count_table("data"),
+                        ),
+                        status = dict(
+                            data = client.load_table_from_duckdb(
+                                connection = conn,
+                                source_table = "data",
+                                target_table = tables["data"],
+                                progress = False,
+                            ),
+                        ),
+                    )
 
-    etl_searchad_report.partial(variables=read_variables()).expand(queries=read_queries())
+
+        (etl_searchad_report
+            .partial(variables=read_searchad_variables())
+            .expand(queries=read_searchad_queries()))
+
+
+    with TaskGroup(group_id="gfa_group") as gfa_group:
+
+        GFA_PATH = ["searchad", "gfa", "searchad_report"]
+
+        @task(task_id="read_gfa_variables", retries=3, retry_delay=timedelta(minutes=1), trigger_rule=TriggerRule.ALWAYS)
+        def read_gfa_variables() -> dict:
+            from variables import read
+            return read(GFA_PATH, tables=True, service_account=True)
+
+        @task(task_id="read_gfa_credentials", retries=3, retry_delay=timedelta(minutes=1), trigger_rule=TriggerRule.ALWAYS)
+        def read_gfa_credentials() -> list:
+            from variables import read
+            return read(GFA_PATH, credentials=True)["credentials"]
+
+
+        @task(task_id="etl_gfa_report", map_index_template="{{ queries['account_no'] }}")
+        def etl_gfa_report(credentials: dict, variables: dict, data_interval_end: pendulum.DateTime = None, **kwargs) -> dict:
+            date = str(data_interval_end.in_timezone("Asia/Seoul").subtract(days=1).date())
+            return main(**credentials, date=date, **variables)
+
+        def main(
+                account_no: int | str,
+                cookies: str,
+                date: str,
+                service_account: dict,
+                tables: dict[str,str],
+                **kwargs
+            ) -> dict:
+            from linkmerce.common.load import DuckDBConnection
+            from linkmerce.api.searchad.gfa import performance_report
+            from linkmerce.extensions.bigquery import BigQueryClient
+
+            with DuckDBConnection(tzinfo="Asia/Seoul") as conn:
+                performance_report(
+                    account_no = account_no,
+                    cookies = cookies,
+                    start_date = date,
+                    connection = conn,
+                    progress = False,
+                    return_type = "none",
+                )
+
+                with BigQueryClient(service_account) as client:
+                    return dict(
+                        params = dict(
+                            account_no = account_no,
+                            date = date,
+                        ),
+                        count = dict(
+                            data = conn.count_table("data"),
+                        ),
+                        status = dict(
+                            data = client.load_table_from_duckdb(
+                                connection = conn,
+                                source_table = "data",
+                                target_table = tables["data"],
+                                progress = False,
+                            ),
+                        ),
+                    )
+
+
+        (etl_gfa_report
+            .partial(variables=read_gfa_variables())
+            .expand(credentials=read_gfa_credentials()))
+
+
+    searchad_group >> gfa_group
