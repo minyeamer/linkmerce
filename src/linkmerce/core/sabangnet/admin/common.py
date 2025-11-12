94c49161c4571be79849cea6d767c1aa4c9b4c46
minyeamer <minyeamer@gmail.com>
2025-11-12 23:35:49 +0900

    update: login process for sabangnet changes

--- src/linkmerce/core/sabangnet/admin/common.py
+++ src/linkmerce/core/sabangnet/admin/common.py
@@ -14,7 +14,8 @@ class SabangnetAdmin(Extractor):
     main_url: str = "https://www.sabangnet.co.kr"
     admin_url: str = "http://sbadmin{domain}.sabangnet.co.kr"
     path: str | None = None
-    token: str = str()
+    access_token: str = str()
+    refresh_token: str = str()
 
     def set_variables(self, variables: Variables = dict()):
         try:
@@ -36,48 +37,43 @@ class SabangnetAdmin(Extractor):
     def with_token(func):
         @functools.wraps(func)
         def wrapper(self: SabangnetAdmin, *args, **kwargs):
-            self.login_begin()
-            self.set_token()
-            self.login_redirect()
+            data = self.login_begin()
+            self.set_token(**data)
+            self.login_history()
             return func(self, *args, **kwargs)
         return wrapper
 
-    def login_begin(self):
+    def login_begin(self) -> dict:
         from linkmerce.utils.headers import build_headers
-        url = self.main_url + "/login.php"
-        body = {"mode": "acts", "id": self.get_variable("userid"), "passwd": self.get_variable("passwd")}
-        headers = build_headers(host=url, contents=dict(type="form"), referer=self.main_url, origin=self.main_url, https=True)
-        self.request("POST", url, data=self.encode_data(body), headers=headers)
+        url = self.main_url + "/hp-prod/users/login"
+        referer = self.main_url + "/login/login-main"
+        body = {"username": self.get_variable("userid"),"password": self.get_variable("passwd")}
+        headers = build_headers(host=url, contents="json", referer=referer, origin=self.main_url)
+        headers["program-name"] = "login-main"
+        with self.request("POST", url, json=body, headers=headers) as response:
+            return response.json()["data"]
 
-    def login_redirect(self):
+    def set_token(self, accessToken: str, refreshToken: str, **kwargs):
+        self.access_token = accessToken
+        self.refresh_token = refreshToken
+
+    def login_history(self):
         from linkmerce.utils.headers import build_headers
-        url = self.main_url + "/new_index.html"
-        body = {
-            "referr": "-1",
-            "returl": "/index.html",
-            "mode": "login",
-            "id": self.get_variable("userid"),
-            "passwd": self.get_variable("userid"),
-            "admin_fg": " ",
-        }
-        headers = build_headers(host=url, contents=dict(type="form"), referer=(self.main_url + "/login.php"), origin=self.main_url, https=True)
-        self.request("POST", url, data=self.encode_data(body), headers=headers)
-
-    def encode_data(self, data: dict, sep='&') -> str:
-        return str(sep).join([f"{k}={v}" for k,v in data.items()])
-
-    def set_token(self):
-        self.token = self.get_session().cookies.get("token")
+        url = self.main_url + "/hp-prod/users/login-history"
+        referer = self.main_url + "/login/login-main"
+        headers = build_headers(host=url, authorization=self.get_authorization(), referer=referer, origin=self.main_url)
+        headers["program-name"] = "login-main"
+        self.request("POST", url, headers=headers)
 
     def get_authorization(self) -> str:
-        return "Bearer " + self.token
+        return "Bearer " + self.access_token
 
 
 class SabangnetLogin(LoginHandler, SabangnetAdmin):
 
     @LoginHandler.with_session
     def login(self, **kwargs) -> dict:
-        self.login_begin()
-        self.set_token()
-        self.login_redirect()
-        return dict(cookies=self.get_cookies(), token=self.token)
+        data = self.login_begin()
+        self.set_token(**data)
+        self.login_history()
+        return dict(cookies=self.get_cookies(), access_token=self.access_token, refresh_token=self.refresh_token)
