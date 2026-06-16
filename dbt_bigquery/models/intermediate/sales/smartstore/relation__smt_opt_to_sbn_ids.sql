e601e0783dd3aff9106989cb9bae71167dd4912d
minyeamer <minyeamer@gmail.com>
2026-06-17 01:14:38 +0900

    fix(dbt): match smartstore options by option_id and expand sabangnet set bundles

--- dbt_bigquery/models/intermediate/sales/smartstore/relation__smt_opt_to_sbn_ids.sql
+++ dbt_bigquery/models/intermediate/sales/smartstore/relation__smt_opt_to_sbn_ids.sql
@@ -14,7 +14,7 @@ smt_opt_to_sbn_ids AS (
   FROM {{ source('relation', 'smt_opt_to_sbn_ids') }}
 ),
 
-option_base AS (
+options_without_sbn_ids AS (
   SELECT
       opt.product_id
     , opt.option_id
@@ -29,9 +29,9 @@ option_base AS (
 
 option_type_0 AS (
   SELECT
-      opt.option_id AS option_id_shop
-    , map.product_id AS product_id_sbn
-  FROM option_base AS opt
+      opt.option_id AS option_id_smt
+    , CONCAT(map.product_id, '-0001') AS option_id_sbn
+  FROM options_without_sbn_ids AS opt
   INNER JOIN {{ source('sabangnet', 'mapping_id') }} AS map
     ON (map.shop_id = 'shop0055') AND (CAST(opt.product_id AS STRING) = map.product_id_shop)
   WHERE opt.product_type = 0
@@ -39,62 +39,57 @@ option_type_0 AS (
 
 option_type_1 AS (
   SELECT
-      opt.option_id AS option_id_shop
-    , SPLIT(map.option_id, '-')[SAFE_OFFSET(0)] AS product_id_sbn
-  FROM option_base AS opt
+      opt.option_id AS option_id_smt
+    , map.option_id AS option_id_sbn
+  FROM options_without_sbn_ids AS opt
   INNER JOIN {{ source('sabangnet', 'mapping_name') }} AS map
     ON (map.shop_id = 'shop0055')
       AND (CAST(opt.product_id AS STRING) = map.product_id_shop)
       AND (CASE
-        WHEN map.sku_name LIKE '%$수량$%'
-        THEN REGEXP_CONTAINS(
+        WHEN map.sku_name LIKE '%$수량$%' THEN REGEXP_CONTAINS(
             opt.option_name
           , REPLACE(
               REGEXP_REPLACE(
                 REGEXP_REPLACE(
                   REGEXP_REPLACE(
-                        REPLACE(map.sku_name, '$수량$', '__수량__')
-                      , r'([\*\+\?\$\^\.\[\]\{\}\(\)\|])', r'\\\1'
-                    ) , '^__수량__', r'[^/]*'
-                  ) , '__수량__$', r'[^/]*'
-              ) , '__수량__', r'[^/]* / [^/]*'
+                      REPLACE(map.sku_name, '$수량$', '__수량__')
+                    , r'([\*\+\?\$\^\.\[\]\{\}\(\)\|])', r'\\\1'
+                  )
+                  , '^__수량__', r'[^/]*'
+                )
+                , '__수량__$', r'[^/]*'
+              )
+              , '__수량__', r'[^/]* / [^/]*'
             )
           )
-        ELSE opt.option_name = map.sku_name END)
+        ELSE opt.option_name = map.sku_name
+      END)
   WHERE opt.product_type = 1
 ),
 
 option_type_2 AS (
   SELECT
-      opt.option_id AS option_id_shop
-    , SPLIT(prd.option_id, '-')[SAFE_OFFSET(0)] AS product_id_sbn
+      opt.option_id AS option_id_smt
+    , prd.option_id AS option_id_sbn
   FROM (
     SELECT
         option_id
       , REGEXP_EXTRACT(product_name, r'^\[([^\]]+)\]') AS group_id
       , TRIM(REGEXP_REPLACE(product_name, r'^\[[^\]]+\]', '')) AS product_name
       , option_name
-    FROM option_base
+    FROM options_without_sbn_ids
     WHERE product_type = 2
   ) AS opt
   INNER JOIN {{ source('sabangnet', 'add_product') }} AS prd
     ON ((opt.group_id = prd.group_id) AND (opt.product_name = prd.option_name)
-    OR (REPLACE(opt.option_name, ' ', '') = CONCAT(REPLACE(prd.group_name, ' ', ''), ':', REPLACE(prd.option_name, ' ', ''))))
+      OR (REPLACE(opt.option_name, ' ', '') = CONCAT(REPLACE(prd.group_name, ' ', ''), ':', REPLACE(prd.option_name, ' ', ''))))
 ),
 
-sabangnet_product AS (
+auto_matched_bundle_ids AS (
   SELECT
-      SPLIT(option_id, '-')[SAFE_OFFSET(0)] AS product_id
-    , REGEXP_REPLACE(bundle_option_ids, '-[0-9]{4}', '') AS bundle_product_ids
-  FROM {{ source('sabangnet', 'option') }}
-)
-
-(SELECT * FROM smt_opt_to_sbn_ids)
-UNION ALL
-(
-  SELECT
-      smt.option_id_shop AS option_id
-    , COALESCE(sbn.bundle_product_ids, CONCAT(smt.product_id_sbn, ':1')) AS bundle_product_ids
+      smt.option_id_smt
+    , smt.option_id_sbn
+    , REGEXP_REPLACE(COALESCE(opt.bundle_option_ids, CONCAT(smt.option_id_sbn, ':1')), '-[0-9]{4}', '') AS bundle_product_ids
   FROM (
     (SELECT * FROM option_type_0)
     UNION ALL
@@ -102,6 +97,24 @@ UNION ALL
     UNION ALL
     (SELECT * FROM option_type_2)
   ) AS smt
-  LEFT JOIN sabangnet_product AS sbn
-    ON smt.product_id_sbn = sbn.product_id
+  LEFT JOIN {{ source('sabangnet', 'option') }} AS opt
+    ON smt.option_id_sbn = opt.option_id
 )
+
+SELECT
+    option_id
+  , bundle_product_ids
+FROM smt_opt_to_sbn_ids
+
+UNION ALL
+
+SELECT
+    option_id_smt AS option_id
+  , bundle_product_ids
+FROM auto_matched_bundle_ids
+QUALIFY ROW_NUMBER() OVER (
+  PARTITION BY option_id_smt
+  ORDER BY
+      ARRAY_LENGTH(SPLIT(bundle_product_ids, ',')) ASC
+    , option_id_sbn ASC
+) = 1
